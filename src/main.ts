16a1760f05e1e94a0d8f41783e0ef16b45644edb
deniz-blue <43997085+deniz-blue@users.noreply.github.com>
2026-01-06 10:13:50 +0200

    initial version

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,75 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { EventData, EventDataSchema } from "@evnt/schema";
+import { globSync, readdirSync, writeFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { findNodeAtLocation, parseTree } from "jsonc-parser";
 
-/**
- * The main function for the action.
- *
- * @returns Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+	try {
+		core.info("@deniz-blue/action-events-repo is running")
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+		const files = globSync("**/*.json", {
+			withFileTypes: true,
+			cwd: core.getInput("events-path") || "./events",
+			exclude: ["node_modules/**", ".github/**", "dist/**", ".ls"],
+		});
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+		core.info(`Found ${files.length} events.`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+		let errorCount = 0;
+		const events = new Map<string, EventData>();
+
+		await Promise.all(
+			files.map(async (dirent) => {
+				const path = join(dirent.parentPath, dirent.name);
+				if (!dirent.isFile()) {
+					if (dirent.isDirectory()) writeFileSync(".ls", JSON.stringify(readdirSync(path), null, 2));
+					return;
+				};
+
+				const content = await readFile(path, "utf-8");
+				try {
+					const result = EventDataSchema.safeParse(JSON.parse(content));
+					if (!result.success) {
+						errorCount++;
+						const json = parseTree(content);
+						if (json == null) return;
+						for (const issue of result.error.issues) {
+							const node = findNodeAtLocation(json, issue.path as (string | number)[]);
+							core.error(issue.message, {
+								file: path,
+								startLine: node?.offset != null ? getPosition(content, node.offset).line + 1 : undefined,
+								startColumn: node?.offset != null ? getPosition(content, node.offset).column + 1 : undefined,
+								endLine: node?.length != null ? getPosition(content, node.offset + node.length).line + 1 : undefined,
+								endColumn: node?.length != null ? getPosition(content, node.offset + node.length).column + 1 : undefined,
+							});
+						}
+					} else {
+						events.set(path, result.data);
+						core.info(`Validated event: ${path}`);
+					}
+				} catch (e) {
+					errorCount++;
+					core.error(`Failed to parse JSON in file: ${path}`, { file: path });
+				}
+			})
+		);
+
+		if (errorCount > 0) {
+			core.setFailed(`Event data validation failed for ${errorCount} event(s).`);
+			return;
+		}
+
+		core.info("Complete")
+	} catch (error) {
+		if (error instanceof Error) core.setFailed(error.message)
+	}
 }
+
+export const getPosition = (text: string, offset: number) => {
+	return {
+		line: text.slice(0, offset).split('\n').length - 1,
+		column: offset - text.lastIndexOf('\n', offset - 1) - 1,
+	};
+};
